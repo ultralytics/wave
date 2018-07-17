6ec9ebac5edb9168c8b87da0db23c957116f20fc
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-07-17 19:25:07 +0200

    updates

--- gcp/vminstall.sh
+++ gcp/vminstall.sh
@@ -20,33 +20,48 @@ google-drive-ocamlfuse drive
 sudo apt install -y git unzip python3-pip screen vim
 
 # Install Python Packages
-pip3 install http://download.pytorch.org/whl/cu91/torch-0.4.0-cp36-cp36m-linux_x86_64.whl
-pip3 install torchvision
 pip3 install -U numpy scipy #tensorflow # plotly  # wave
 pip3 install -U opencv-python exifread tqdm # bokeh  # velocity
+pip3 install http://download.pytorch.org/whl/cu91/torch-0.4.0-cp36-cp36m-linux_x86_64.whl  # CUDA 9.1
+# pip3 install http://download.pytorch.org/whl/cu90/torch-0.4.0-cp36-cp36m-linux_x86_64.whl  # CUDA 9.0
+pip3 install torchvision
 
 # GPU driver install P100 and K80
-#sudo apt install ubuntu-drivers-common -y
-# sudo ubuntu-drivers autoinstall
+sudo apt install python3-opencv
+sudo apt install ubuntu-drivers-common -y
+sudo ubuntu-drivers autoinstall
 
 # GPU driver install on *Ubuntu 16.04 LTS* from https://cloud.google.com/compute/docs/gpus/add-gpus#install-driver-script
-echo "Checking for CUDA and installing."
-# Check for CUDA and try to install.
-if ! dpkg-query -W cuda-9-0; then
-# The 16.04 installer works with 16.10.
-curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-apt-get update
-apt-get install cuda-9-0 -y
-fi
-# Enable persistence mode
-nvidia-smi -pm 1
+# echo "Checking for CUDA and installing."
+# if ! dpkg-query -W cuda-9-0; then
+# sudo curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+# sudo dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+# sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+# sudo apt-get update
+# sudo apt-get install cuda-9-0 -y
+# fi
+# # Enable persistence mode
+# sudo nvidia-smi -pm 1
+
+
+# get xview training data
+wget -O train_images.tgz 'https://d307kc0mrhucc3.cloudfront.net/train_images.tgz?Expires=1531850600&Signature=FvYp1qDdadcfOAF2ELmHSJXaRYkq~R2KrRi0Fk3akL1~UZWiCm26QjLh52e11Ga-99GNAkLylXauCgt0k17hmw2aMrMDW-z9Qo9hGQV-BkYEimhd~dyybOqqGJ3ZWG3CmeesHHJ7ScdDpv9aIxZTNo-QUSABA8g5X2oMs96RWOy-GnAw09W8liBIoLAfeoGcqOubvY7vOMtFeFgFatzmMSoLPQ-Y8Zv2bGpQyih-pd7A2S0VAE3ccDwvjKgdOgYeuZLXBNaF5Wy~-JNX2RdaqaXmLO42P3soxT5FnCnGbLYoVAI7K6-mtlttcw0VOTMXqWvoN8QOsdZenREhKfJ0iw__&Key-Pair-Id=APKAIKGDJB5C3XUL2DXQ'
+tar -xvzf train_images.tgz
+sudo rm -rf train_images/._*
+
+# convert all .tif to .bmp
+sudo rm -rf train_images/659.tif train_images/769.tif
+sudo rm -rf yolo
+git clone https://github.com/ultralytics/yolo
+cd yolo
+python3
+from utils import datasets
+datasets.convert_tif2bmp('../train_images')
+exit()
 
 # Shutdown
 sudo apt autoremove -y
 rm -rf vminstall.bash
 sudo shutdown now
 
-# Extras
-# find MATLAB/ -size +100M -ls
+

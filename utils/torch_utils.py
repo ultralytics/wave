d84bc9c272bee3334d90021508ad7e7c0c94e20e
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-04-08 15:35:01 +0200

    updates

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -14,10 +14,10 @@ def select_device(force_cpu=False):
     if not cuda:
         print('Using CPU')
     if cuda:
+        c = 1024 ** 2  # bytes to MB
         ng = torch.cuda.device_count()
         x = [torch.cuda.get_device_properties(i) for i in range(ng)]
         print("Using CUDA device0 (name='%s', total_memory=%gMB)" % (x[0].name, x[0].total_memory/c))
-        c = 1024 ** 2  # bytes to MB
         if ng > 0:
             # torch.cuda.set_device(0)  # OPTIONAL: Set your GPU if multiple available
             for i in range(1, ng):

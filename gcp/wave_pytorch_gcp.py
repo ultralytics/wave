938905c17fb583e0b06230e97752585e8ef32ff6
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-06-22 13:06:02 +0200

    Refactor code for speed and clarity

--- gcp/wave_pytorch_gcp.py
+++ gcp/wave_pytorch_gcp.py
@@ -4,7 +4,6 @@ import time
 
 import scipy.io
 import torch
-
 from utils import *
 
 # set printoptions
--- train_tf.py
+++ train_tf.py
@@ -5,7 +5,6 @@ import plotly.graph_objs as go
 import scipy.io
 import tensorflow as tf
 from plotly.offline import plot
-
 from utils.utils import *
 
 tf.enable_eager_execution()
